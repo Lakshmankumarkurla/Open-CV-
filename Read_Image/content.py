a6f1139a31f34a873c54eba59be85851267e9c69
kurla jaya laxman kumar <93817346+Lakshmankumarkurla@users.noreply.github.com>
2023-02-08 20:40:31 +0530

    Update content.py

--- Read_Image/content.py
+++ Read_Image/content.py
@@ -1,1 +1,8 @@
+import cv2 as cv
 
+img = cv.imread(r'C:\Users\JayaLakshman\opencv\cat.jpg',)
+cv.imshow('cat',img)
+k = cv.waitKey(0) & 0XFF
+print(img.shape)
+if k==27:
+    cv.destroyAllWindows()
